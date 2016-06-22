c84ceab2d8634e0ed3166eb312c1fe470f8a88de
heiheiha <1137029429@qq.com>
2016-06-22 16:46:06 +0800

    textxyz
    textxyz,第一次练习，简单的x+y＝z

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  textxyz
+//
+//  Created by  黄磊 on 16/6/22.
+//  Copyright © 2016年  黄磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
